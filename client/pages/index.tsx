a74d4fe33887b13a10019095312cdb3a698d5b10
irsat000 <irsat000@gmail.com>
2023-10-15 00:39:29 +0300

    Changed intro

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,20 +8,17 @@ export default function Home() {
     <Template>
       <>
         <div className="intro">
-          <div className="customer">
-            <h2>Müşteri</h2>
+          <div className="intro-content">
+            <h2>Zanaat sanattır, ödemesini bilmeli.</h2>
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem animi reiciendis debitis beatae temporibus, voluptatibus earum molestiae. Dolores tempora consectetur ad possimus veritatis quibusdam aspernatur quod natus unde quas. Culpa.
             </p>
-            <button>İlan ver</button>
+            <div className="intro-buttons">
+              <button>İlan ver</button>
+              <button>İş ara</button>
+            </div>
           </div>
-          <div className="freelancer">
-            <h2>Serbest Çalışan <span>(Freelancer)</span></h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellat labore asperiores, a nihil ipsam nisi natus rerum! Officiis ipsam asperiores vero quam aliquam, sequi totam iste est corporis error!
-            </p>
-            <button>İş ara</button>
-          </div>
+          <Image src={require('./assets/site/painter2.jpg')} className='intro-image' alt={''} />
         </div>
       </>
     </Template>
