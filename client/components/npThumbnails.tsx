f729767e8b6c5b78c4650f95f5771a735d23d062
irsat000 <irsat000@gmail.com>
2023-12-25 03:42:18 +0300

    Fixed drag indicator's delay in new post page

--- client/components/npThumbnails.tsx
+++ client/components/npThumbnails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import { XLg } from "react-bootstrap-icons";
 
@@ -16,41 +16,54 @@ export const NP_Thumbnails: React.FC<{
     setFormData: React.Dispatch<React.SetStateAction<NPFormData>>
 }> = ({ formData, setFormData }) => {
     // Drag information
-    const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+    const hoverIndex = useRef<number | null>(null);
+    const dragIndicator = useRef<HTMLSpanElement | null>(null);
     const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
 
     // Get the dragged index and replace the hovered index
     const handleDragEnd = (index: number) => {
-        if (hoverIndex == null) return;
+        if (hoverIndex.current == null) return;
         // Make copy
         const updatedImages = [...formData.selectedImages];
         // Get the dragged image
         const [draggedImage] = updatedImages.splice(index, 1);
         // Update the copy
-        updatedImages.splice(hoverIndex, 0, draggedImage);
+        updatedImages.splice(hoverIndex.current, 0, draggedImage);
         // Update the formData
         setFormData({
             ...formData,
             selectedImages: updatedImages
         });
         // Reset drag information
-        setHoverIndex(null);
+        hoverIndex.current = null;
         setDraggedIndex(null);
     };
 
+    // Change indicator's order to match with the user's mouse movement
+    const handleDragIndicatorOrder = (order: number) => {
+        if (!dragIndicator.current) return
+        dragIndicator.current.style.order = order.toString()
+    }
+
     return (
         <div className="np-thumbnail-wrapper">
-            <span className={`drag-indicator ${hoverIndex != null ? 'active' : ''}`} style={{ order: hoverIndex ?? 'unset' }}></span>
+            <span ref={dragIndicator} className={`drag-indicator ${draggedIndex != null ? 'active' : ''}`}></span>
             {formData.selectedImages.length > 0 ? formData.selectedImages.map((image, index) => (
                 <div key={index} className={`image-thumbnail ${draggedIndex == index ? 'dragged' : ''}`}
                     style={{ order: index }}
                     draggable="true"
-                    onDragStart={() => setDraggedIndex(index)}
+                    onDragStart={() => {
+                        setDraggedIndex(index)
+                        handleDragIndicatorOrder(index)
+                    }}
                     onDragEnter={() => {
-                        if (hoverIndex !== index)
-                            setHoverIndex(index)
+                        if (hoverIndex.current !== index) {
+                            hoverIndex.current = index
+                            handleDragIndicatorOrder(index)
+                        }
                     }}
                     onDragEnd={() => handleDragEnd(index)}
+                    onDragOver={(e) => e.preventDefault()}
                 >
                     <div className="image-wrapper">
                         <img src={URL.createObjectURL(new Blob([image]))} alt={`Image ${index}`} />
