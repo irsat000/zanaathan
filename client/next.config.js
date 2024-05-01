c792346b09847079bcdd4fd95f9c9ad8fb186970
irsat000 <irsat000@gmail.com>
2024-05-01 20:25:38 +0300

    redirect www to non www

--- client/next.config.js
+++ client/next.config.js
@@ -1,29 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
+  redirects: async () => {
     return [
-      // Redirect http://site.com/* to https://www.site.com/*
       {
-        source: 'http://zanaathan.com/:path*',
-        destination: 'https://zanaathan.com/:path*',
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.zanaathan.com',
+          },
+        ],
         permanent: true,
-      },
-      // Redirect https://site.com/* to https://www.site.com/*
-      {
-        source: 'http://www.zanaathan.com/:path*',
         destination: 'https://zanaathan.com/:path*',
-        permanent: true,
       },
-      // Redirect https://www.site.com/* to https://www.site.com/*
-      // This ensures www vs non-www consistency
-      {
-        source: 'https://www.zanaathan.com/:path*',
-        destination: 'https://zanaathan.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+    ]
+  }
 }
 
 module.exports = nextConfig
